3572f0cb5db67eeb7d2f3799f640fa0621a6bc1d
Jazmin M <jazminmerckel@gmail.com>
2019-09-22 03:41:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase
     return "HUH?! SPEAK UP, SONNY!"
   else phrase.upcase
