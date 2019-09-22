e5902612f7407a209ce9de660943b2bb35615d13
Jazmin M <jazminmerckel@gmail.com>
2019-09-22 03:13:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  
+  if "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU PUMPKIN"
+  elsif 
 end
 
 # Whatever you say to grandma, she should respond with
