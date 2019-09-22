c47892ba0c231923fc7178ca9143c914e0c33745
Jazmin M <jazminmerckel@gmail.com>
2019-09-22 03:43:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,10 +2,9 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase
-    return "HUH?! SPEAK UP, SONNY!"
-  else phrase.upcase
+  elsif phrase.upcase
     return "NO, NOT SINCE 1938!"
+  
   end
 end
 
